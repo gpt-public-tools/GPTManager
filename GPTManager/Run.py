c64bf1fad4604dda4aed7451f11fd449035c1a5d
ilketorun <ilketorun97@gmail.com>
2023-11-28 00:27:07 +0300

    Run and Thread update

--- GPTManager/Run.py
+++ GPTManager/Run.py
@@ -8,10 +8,30 @@ class Tool:
     type: str
     function: object = None
     
+class RunObject:
+    __id: str
+    __object: str = "thread.run"
+    __created_at: int
+    __assistant_id: str
+    __thread_id: str
+    __status: str
+    __started_at: int
+    __expires_at: Optional[int]
+    __cancelled_at: Optional[int]
+    __failed_at: Optional[int]
+    __completed_at: Optional[int]
+    __last_error: Optional[str]
+    __model: str = "gpt-4-1106-preview"
+    __instructions: Optional[str]
+    __tools: list[Tool] = [{"type": "retrieval"}, {"type": "code_interpreter"}]
+    __file_ids: list[Any] = field(default_factory=list)
+    __metadata: Dict[str, Any] = field(default_factory=dict)
+
 @dataclass
 class Run:
     """
     A class representing a thread run.
+
     Attributes:
         id (str): The run id.
         object (str): The object type.
@@ -30,53 +50,57 @@ class Run:
         tools (list[Tool]): The tools used for the run.
         file_ids (list[Any]): The file ids used for the run.
         metadata (Dict[str, Any]): The metadata for the run.
-    """
-    id: str
-    object: str
-    created_at: int
-    assistant_id: str
-    thread_id: str
-    status: str
-    started_at: int
-    expires_at: Optional[int]
-    cancelled_at: Optional[int]
-    failed_at: Optional[int]
-    completed_at: Optional[int]
-    last_error: Optional[str]
-    model: str
-    instructions: Optional[str]
-    tools: list[Tool]
-    file_ids: list[Any] = field(default_factory=list)
-    metadata: Dict[str, Any] = field(default_factory=dict)
+    """  
+    __id: str
+    __object: str = "thread.run"
+    __created_at: int
+    __assistant_id: str
+    __thread_id: str
+    __status: str
+    __started_at: int
+    __expires_at: Optional[int]
+    __cancelled_at: Optional[int]
+    __failed_at: Optional[int]
+    __completed_at: Optional[int]
+    __last_error: Optional[str]
+    __model: str = "gpt-4-1106-preview"
+    __instructions: Optional[str]
+    __tools: list[Tool] = [{"type": "retrieval"}, {"type": "code_interpreter"}]
+    __file_ids: list[Any] = field(default_factory=list)
+    __metadata: Dict[str, Any] = field(default_factory=dict)
 
     def __init__(self, thread_id: str|None = None, assistant_id: str|None = None, run_id: str|None = None) -> None:
         """
         Creates a new thread run or retrieves an existing one.
+
         Args:
             thread_id (str): The thread id.
             assistant_id (str): The assistant id.
             run_id (str): The run id.
+
         Returns:
             None
+
         Raises:
             ValueError: If the thread_id or assistant_id is not set.
         """
         if run_id is not None and thread_id is not None:
-            self.id = run_id
-            self.thread_id = thread_id
+            self.__id = run_id
+            self.__thread_id = thread_id
             self.retrieve_run()
         elif thread_id is not None and assistant_id is not None:
-            self.assistant_id = assistant_id
-            self.thread_id = thread_id
+            self.__assistant_id = assistant_id
+            self.__thread_id = thread_id
             self.create_run()
         
-        pass
 
     def create_run(self) -> None:
         """
         Creates a new thread run.
+
         Returns:
             None
+
         Raises:
             ValueError: If the thread_id or assistant_id is not set.
         """
@@ -84,25 +108,25 @@ class Run:
 
         try:
             run = client.beta.threads.runs.create(
-                thread_id=self.thread_id,
-                assistant_id=self.assistant_id,
+                thread_id=self.__thread_id,
+                assistant_id=self.__assistant_id,
             )
 
-            self.id = run.id
-            self.object = run.object
-            self.created_at = run.created_at
-            self.status = run.status
-            self.started_at = run.started_at
-            self.expires_at = run.expires_at
-            self.cancelled_at = run.cancelled_at
-            self.failed_at = run.failed_at
-            self.completed_at = run.completed_at
-            self.last_error = run.last_error
-            self.model = run.model
-            self.instructions = run.instructions
-            self.tools = run.tools
-            self.file_ids = run.file_ids
-            self.metadata = run.metadata
+            self.__id = run.id
+            self.__object = run.object
+            self.__created_at = run.created_at
+            self.__status = run.status
+            self.__started_at = run.started_at
+            self.__expires_at = run.expires_at
+            self.__cancelled_at = run.cancelled_at
+            self.__failed_at = run.failed_at
+            self.__completed_at = run.completed_at
+            self.__last_error = run.last_error
+            self.__model = run.model
+            self.__instructions = run.instructions
+            self.__tools = run.tools
+            self.__file_ids = run.file_ids
+            self.__metadata = run.metadata
             
         except Exception as e:
             raise ValueError(e)
@@ -110,8 +134,10 @@ class Run:
     def retrieve_run(self):
         """
         Retrieves a thread run.
+
         Returns:
             None
+
         Raises:
             ValueError: If the thread_id or assistant_id is not set.
         """
@@ -119,25 +145,24 @@ class Run:
 
         try:
             run = client.beta.threads.runs.retrieve(
-                thread_id=self.thread_id,
-                run_id=self.id,
+                thread_id=self.__thread_id,
+                run_id=self.__id,
             )
 
-            self.id = run.id
-            self.object = run.object
-            self.created_at = run.created_at
-            self.status = run.status
-            self.started_at = run.started_at
-            self.expires_at = run.expires_at
-            self.cancelled_at = run.cancelled_at
-            self.failed_at = run.failed_at
-            self.completed_at = run.completed_at
-            self.last_error = run.last_error
-            self.model = run.model
-            self.instructions = run.instructions
-            self.tools = run.tools
-            self.file_ids = run.file_ids
-            self.metadata = run.metadata
+            self.__object = run.object
+            self.__created_at = run.created_at
+            self.__status = run.status
+            self.__started_at = run.started_at
+            self.__expires_at = run.expires_at
+            self.__cancelled_at = run.cancelled_at
+            self.__failed_at = run.failed_at
+            self.__completed_at = run.completed_at
+            self.__last_error = run.last_error
+            self.__model = run.model
+            self.__instructions = run.instructions
+            self.__tools = run.tools
+            self.__file_ids = run.file_ids
+            self.__metadata = run.metadata
 
         except Exception as e:
             raise ValueError(e)
@@ -145,10 +170,13 @@ class Run:
     def modify_run(self, metadata) -> None:
         """
         Modifies a thread run.
+
         Args:
             **kwargs: The attributes to modify.
+
         Returns:
             None
+
         Raises:
             ValueError: If the thread_id or assistant_id is not set.
         """
@@ -156,45 +184,31 @@ class Run:
 
         try:
             run = client.beta.threads.runs.update(
-                thread_id=self.thread_id,
-                run_id=self.id,
+                thread_id=self.__thread_id,
+                run_id=self.__id,
                 metadata=metadata
             )
 
-            self.id = run.id
-            self.object = run.object
-            self.created_at = run.created_at
-            self.status = run.status
-            self.started_at = run.started_at
-            self.expires_at = run.expires_at
-            self.cancelled_at = run.cancelled_at
-            self.failed_at = run.failed_at
-            self.completed_at = run.completed_at
-            self.last_error = run.last_error
-            self.model = run.model
-            self.instructions = run.instructions
-            self.tools = run.tools
-            self.file_ids = run.file_ids
-            self.metadata = run.metadata
+            self.__metadata = run.metadata
 
         except Exception as e:
             raise ValueError(e)
         
-    def list_runs(self) -> list[Run]:
+    def list_runs(self) -> list[RunObject]:
         """
         Lists all thread runs.
         Returns:
-            None
+            list[RunObject]: A list of RunObject instances representing each run.
         Raises:
-            ValueError: If the thread_id or assistant_id is not set.
+            ValueError: If the thread_id or assistant_id is not set, or if the API call fails.
         """
         client = OpenAI()
 
         try:
-            runs = client.beta.threads.runs.list(
-                thread_id=self.thread_id
+            runs_data = client.beta.threads.runs.list(
+                thread_id=self.__thread_id
             )
-
+            runs = [RunObject(**run_data) for run_data in runs_data]  # Convert each dict to a RunObject
             return runs
 
         except Exception as e:
--- GPTManager/Thread.py
+++ GPTManager/Thread.py
@@ -34,87 +34,79 @@ class MessageFileObject:
     message_id: str
     file_id: str
 
-@dataclass
-class ThreadObject:
-    id: str
-    object: str
-    created_at: int
-    metadata: Dict[str, Any] = field(default_factory=dict)
-    
 class Thread:
-    __thread: ThreadObject  = None
+    __id: str
+    __object: str
+    __created_at: int
+    __metadata: Dict[str, Any] = field(default_factory=dict)
 
-    def __init__(self, thread_id: str = None):
-        if thread_id:
-            self.retrieve_thread(thread_id)
+
+    def __init__(self, thread_id: str|None = None):
+        if thread_id is not None:
+            self.__id = thread_id
+            self.retrieve_thread()
         else:
             self.create_thread()
 
 
-    @property
-    def thread(self) -> ThreadObject:
-        """Getter for __thread"""
-        return self.__thread
-
-
-    @thread.setter
-    def thread(self, value: ThreadObject):
-        """Setter for __thread"""
-        if not isinstance(value, ThreadObject):
-            raise ValueError("Value must be an instance of ThreadObject")
-        self.__thread = value
-
-  
-    def create_thread(self) -> ThreadObject:
+    def create_thread(self):
         """
-        Creates a new thread using the OpenAI client and sets the __thread attribute.
+        Creates a new thread using the OpenAI client and sets the __id, __object, __created_at and __metadata attributes.
+        
         Returns:
-            ThreadObject: An instance representing the newly created thread.
+            None
+
         Raises:
             ValueError: If the thread creation fails or returns invalid data.
         """
         try:
             client = OpenAI()
             thread_data = client.beta.threads.create()
-            self.thread = ThreadObject(**thread_data)  # Using the setter
-            return self.thread
+            self.__id = thread_data.get('id', None)
+            self.__object = thread_data.get('object', None)
+            self.__created_at = thread_data.get('object', None)
+            self.__metadata = thread_data.get('id', None)
         except Exception as e:
             raise ValueError("Failed to create thread") from e
 
 
-    def retrieve_thread(self, thread_id: str) -> ThreadObject:
+    def retrieve_thread(self):
         """
-        Retrieves the thread from id using the OpenAI client and sets the __thread attribute.
+        Retrieves the thread from id using the OpenAI client and sets the __id, __object, __created_at and __metadata attributes.
+        
         Returns:
-            ThreadObject: An instance representing the retrieved thread.
+            None
+
         Raises:
             ValueError: If the thread retrieval fails or returns invalid data.
         """
         try:
             client = OpenAI()
-            thread_data = client.beta.threads.retrieve(thread_id)
-            self.thread = ThreadObject(**thread_data)  # Using the setter
-            return self.thread
+            thread_data = client.beta.threads.retrieve(self.__id)
+            self.__object = thread_data.get('object', None)
+            self.__created_at = thread_data.get('object', None)
+            self.__metadata = thread_data.get('id', None)
         except Exception as e:
             raise ValueError("Failed to retreive thread") from e
 
 
-    def modify_thread_metadata(self, thread_id: str, metadata: dict) -> ThreadObject:   
+    def modify_thread(self, metadata: dict):   
         """
-        Modifies the thread  using the OpenAI client and sets the __thread.metadata attribute.
+        Modifies the thread  using the OpenAI client and sets the __metadata attribute.
+        
         Returns:
-            ThreadObject: An instance representing the modified thread.
+            None   
+             
         Raises:
             ValueError: If the thread modification fails or returns invalid data.
         """
         try:
             client = OpenAI()
             thread_data = client.beta.threads.update(
-                thread_id = thread_id, 
+                thread_id = self.__id, 
                 metadata = metadata
             )
-            self.thread = ThreadObject(**thread_data)  # Using the setter
-            return self.thread
+            self.__metadata = thread_data.get('id', None)
         except Exception as e:
             raise ValueError("Failed to modify thread metadata") from e
 
@@ -133,13 +125,69 @@ class Thread:
         """
         try:
             client = OpenAI()
-            self.thread = None
+            self.__id = None
+            self.__object = None
+            self.__created_at = None
+            self.__metadata = None
             return client.beta.threads.delete(thread_id)
         except Exception as e:
             raise ValueError("Failed to delete thread") from e
 
+   
+    def create_message(self, **kwargs) -> MessageObject:
+        """
+        Creates a message for the thread, either from a MessageObject or by specifying individual parameters.
 
-    def create_message_from_params(self, role, content: str, file_ids = []) -> MessageObject:
+        This method can accept a MessageObject directly, or a combination of 'role', 'content', and optionally 'file_ids'.
+        
+        Parameters:
+            kwargs: A dictionary of keyword arguments. This can include:
+                - message_object (MessageObject): An instance of MessageObject containing message details.
+                - role (str): The role of the message sender (required if message_object is not provided).
+                - content (str): The content of the message (required if message_object is not provided).
+                - file_ids (list): A list of file IDs to be attached to the message (optional).
+
+        Returns:
+            MessageObject: An instance representing the created message, or None if required parameters are missing.
+
+        Raises:
+            ValueError: If message creation fails or returns invalid data.
+        """
+        if 'message_object' in kwargs:
+            return self.__create_message_from_message_object(message_object=kwargs['message_object'])
+        elif 'role' in kwargs and 'content' in kwargs:
+            file_ids = kwargs.get('file_ids', [])
+            return self.__create_message_from_params(role=kwargs['role'], content=kwargs['content'], file_ids=file_ids)
+        return None
+
+
+    def __create_message_from_message_object(self, message_object: MessageObject) -> MessageObject:
+        """     
+        Creates a message for the thread using a MessageObject.
+
+        Parameters:
+            message (MessageObject): An instance of MessageObject containing message details.
+
+        Returns:
+            MessageObject: An instance representing the created message.
+
+        Raises:
+            ValueError: If message creation fails or returns invalid data.
+        """
+        try:
+            client = OpenAI()
+            message_data = client.beta.threads.messages.create(
+                message_object.thread_id,
+                role=message_object.role,
+                content=message_object.content,
+                file_ids=message_object.file_ids
+            )
+            return MessageObject(**message_data)
+        except Exception as e:
+            raise ValueError("Failed to create message from MessageObject") from e
+
+
+    def __create_message_from_params(self, role, content: str, file_ids = []) -> MessageObject:
         """     
         Creates a message for the thread using the provided parameters.
 
@@ -166,32 +214,6 @@ class Thread:
         except Exception as e:
             raise ValueError("Failed to create message") from e
     
-    
-    def create_message_from_message_object(self, message: MessageObject) -> MessageObject:
-        """     
-        Creates a message for the thread using a MessageObject.
-
-        Parameters:
-            message (MessageObject): An instance of MessageObject containing message details.
-
-        Returns:
-            MessageObject: An instance representing the created message.
-
-        Raises:
-            ValueError: If message creation fails or returns invalid data.
-        """
-        try:
-            client = OpenAI()
-            message_data = client.beta.threads.messages.create(
-                message.thread_id,
-                role=message.role,
-                content=message.content,
-                file_ids=message.file_ids
-            )
-            return MessageObject(**message_data)
-        except Exception as e:
-            raise ValueError("Failed to create message from MessageObject") from e
-
 
     def retrieve_message(self, message_id: str) -> MessageObject:
         """
